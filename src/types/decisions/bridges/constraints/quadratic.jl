fe4ff94634536f507f7c4b541fd62c16aba97aeb
Daniel Montes <dmontesl1203@gmail.com>
2023-06-23 11:03:14 +0200

    Update errored functions

--- src/types/decisions/bridges/constraints/quadratic.jl
+++ src/types/decisions/bridges/constraints/quadratic.jl
@@ -22,66 +22,66 @@
 
 # Quadratic decision function #
 # ========================== #
-mutable struct QuadraticDecisionConstraintBridge{T, S} <: MOIB.Constraint.AbstractBridge
-    constraint::CI{MOI.ScalarQuadraticFunction{T}, S}
+mutable struct QuadraticDecisionConstraintBridge{T,S} <: MOIB.Constraint.AbstractBridge
+    constraint::CI{MOI.ScalarQuadraticFunction{T},S}
     decision_function::QuadraticDecisionFunction{T}
     set::S
 end
 
 function MOIB.Constraint.bridge_constraint(::Type{QuadraticDecisionConstraintBridge{T,S}},
-                                           model,
-                                           f::QuadraticDecisionFunction{T},
-                                           set::S) where {T, S}
+    model,
+    f::QuadraticDecisionFunction{T},
+    set::S) where {T,S}
     # All decisions have been mapped to the
     # variable part terms at this point.
     # Add the bridged constraint
     constraint = MOI.add_constraint(model,
-                                    MOI.ScalarQuadraticFunction(
-                                        f.variable_part.quadratic_terms,
-                                        f.variable_part.affine_terms,
-                                        zero(T)),
-                                    MOIU.shift_constant(set, -f.variable_part.constant))
+        MOI.ScalarQuadraticFunction(
+            f.variable_part.quadratic_terms,
+            f.variable_part.affine_terms,
+            zero(T)),
+        MOIU.shift_constant(set, -f.variable_part.constant))
     # Save the constraint index, the decision function, and the set, to allow modifications
-    return QuadraticDecisionConstraintBridge{T, S}(constraint, f, set)
+    return QuadraticDecisionConstraintBridge{T,S}(constraint, f, set)
 end
 
 function MOI.supports_constraint(::Type{<:QuadraticDecisionConstraintBridge{T}},
-                                 ::Type{<:QuadraticDecisionFunction{T}},
-                                 ::Type{<:MOI.AbstractScalarSet}) where T
+    ::Type{<:QuadraticDecisionFunction{T}},
+    ::Type{<:MOI.AbstractScalarSet}) where {T}
     return true
 end
 function MOIB.added_constrained_variable_types(::Type{<:QuadraticDecisionConstraintBridge})
     return Tuple{Type}[]
 end
-function MOIB.added_constraint_types(::Type{QuadraticDecisionConstraintBridge{T, S}}) where {T, S}
+function MOIB.added_constraint_types(::Type{QuadraticDecisionConstraintBridge{T,S}}) where {T,S}
     return [(MOI.ScalarQuadraticFunction{T}, S)]
 end
 function MOIB.Constraint.concrete_bridge_type(::Type{<:QuadraticDecisionConstraintBridge{T}},
-                                              ::Type{QuadraticDecisionFunction{T}},
-                                              S::Type{<:MOI.AbstractScalarSet}) where T
-    return QuadraticDecisionConstraintBridge{T, S}
+    ::Type{QuadraticDecisionFunction{T}},
+    S::Type{<:MOI.AbstractScalarSet}) where {T}
+    return QuadraticDecisionConstraintBridge{T,S}
 end
 
-MOI.get(b::QuadraticDecisionConstraintBridge{T, S}, ::MOI.NumberOfConstraints{MOI.ScalarQuadraticFunction{T}, S}) where {T, S} = 1
-MOI.get(b::QuadraticDecisionConstraintBridge{T, S}, ::MOI.ListOfConstraintIndices{MOI.ScalarQuadraticFunction{T}, S}) where {T, S} = [b.constraint]
+MOI.get(b::QuadraticDecisionConstraintBridge{T,S}, ::MOI.NumberOfConstraints{MOI.ScalarQuadraticFunction{T},S}) where {T,S} = 1
+MOI.get(b::QuadraticDecisionConstraintBridge{T,S}, ::MOI.ListOfConstraintIndices{MOI.ScalarQuadraticFunction{T},S}) where {T,S} = [b.constraint]
 
 function MOI.get(model::MOI.ModelLike, attr::MOI.ConstraintFunction,
-                 bridge::QuadraticDecisionConstraintBridge{T,S}) where {T,S}
+    bridge::QuadraticDecisionConstraintBridge{T,S}) where {T,S}
     return bridge.decision_function
 end
 
 function MOI.get(model::MOI.ModelLike, attr::MOI.ConstraintSet,
-                 bridge::QuadraticDecisionConstraintBridge{T,S}) where {T,S}
+    bridge::QuadraticDecisionConstraintBridge{T,S}) where {T,S}
     return bridge.set
 end
 
-function MOI.get(model::MOI.ModelLike, attr::Union{MOI.ConstraintPrimal, MOI.ConstraintDual},
-                 bridge::QuadraticDecisionConstraintBridge{T,S}) where {T,S}
+function MOI.get(model::MOI.ModelLike, attr::Union{MOI.ConstraintPrimal,MOI.ConstraintDual},
+    bridge::QuadraticDecisionConstraintBridge{T,S}) where {T,S}
     return MOI.get(model, attr, bridge.constraint)
 end
 
 function MOI.get(model::MOI.ModelLike, ::DecisionIndex,
-                 bridge::QuadraticDecisionConstraintBridge{T}) where T
+    bridge::QuadraticDecisionConstraintBridge{T}) where {T}
     return bridge.constraint
 end
 
@@ -91,29 +91,29 @@ function MOI.delete(model::MOI.ModelLike, bridge::QuadraticDecisionConstraintBri
 end
 
 function MOI.set(model::MOI.ModelLike, ::MOI.ConstraintFunction,
-                 bridge::QuadraticDecisionConstraintBridge{T,S},
-                 f::QuadraticDecisionFunction{T}) where {T,S,F}
+    bridge::QuadraticDecisionConstraintBridge{T,S},
+    f::QuadraticDecisionFunction{T}) where {T,S}
     # Update bridge functions and function constant
     bridge.decision_function = f
     # Change the function of the bridged constraints
     MOI.set(model, MOI.ConstraintFunction(), bridge.constraint,
-            MOI.ScalarQuadraticFunction(
-                f.variable_part.quadratic_terms,
-                f.variable_part.affine_terms,
-                zero(T)))
+        MOI.ScalarQuadraticFunction(
+            f.variable_part.quadratic_terms,
+            f.variable_part.affine_terms,
+            zero(T)))
     # Shift constraint set
     MOI.set(model, MOI.ConstraintSet(), bridge.constraint,
-            MOIU.shift_constant(bridge.set, -f.variable_part.constant))
+        MOIU.shift_constant(bridge.set, -f.variable_part.constant))
     return nothing
 end
 
 function MOI.set(model::MOI.ModelLike, ::MOI.ConstraintSet,
-                 bridge::QuadraticDecisionConstraintBridge{T,S}, change::S) where {T,S}
+    bridge::QuadraticDecisionConstraintBridge{T,S}, change::S) where {T,S}
     f = bridge.decision_function
     bridge.set = change
     # Shift constraint set
     MOI.set(model, MOI.ConstraintSet(), bridge.constraint,
-            MOIU.shift_constant(bridge.set, -f.variable_part.constant))
+        MOIU.shift_constant(bridge.set, -f.variable_part.constant))
     return nothing
 end
 
@@ -123,7 +123,7 @@ function MOI.modify(model::MOI.ModelLike, bridge::QuadraticDecisionConstraintBri
     lq.variable_part.constant = change.new_constant
     # Shift constraint set
     MOI.set(model, MOI.ConstraintSet(), bridge.constraint,
-            MOIU.shift_constant(bridge.set, -f.variable_part.constant))
+        MOIU.shift_constant(bridge.set, -f.variable_part.constant))
     return nothing
 end
 
--- src/types/decisions/expressions/mutable_arithmetics.jl
+++ src/types/decisions/expressions/mutable_arithmetics.jl
@@ -24,8 +24,8 @@
 # ========================== #
 const _Variable = VariableRef
 const _Decision = DecisionRef
-const _Scalar = Union{_Variable, _Decision, _Constant}
-const _DecisionAffOrQuadExpr{C} = Union{DecisionAffExpr{C}, DecisionQuadExpr{C}}
+const _Scalar = Union{_Variable,_Decision,_Constant}
+const _DecisionAffOrQuadExpr{C} = Union{DecisionAffExpr{C},DecisionQuadExpr{C}}
 
 # Basic operations #
 # ========================== #
@@ -37,80 +37,80 @@ end
 # Promote operation #
 # ========================== #
 # _Constant--_Decision
-function MA.promote_operation(::Union{typeof(+), typeof(-), typeof(*)},
-                               ::Type{<:_Constant}, S::Type{<:_Decision})
+function MA.promote_operation(::Union{typeof(+),typeof(-),typeof(*)},
+    ::Type{<:_Constant}, S::Type{<:_Decision})
     return DAE
 end
-function MA.promote_operation(::Union{typeof(+), typeof(-), typeof(*)},
-                               S::Type{<:_Decision}, ::Type{<:_Constant})
+function MA.promote_operation(::Union{typeof(+),typeof(-),typeof(*)},
+    S::Type{<:_Decision}, ::Type{<:_Constant})
     return DAE
 end
 # _Variable--Decision
-function MA.promote_operation(::Union{typeof(+), typeof(-)},
-                               ::Type{<:_Variable}, S::Type{<:_Decision})
+function MA.promote_operation(::Union{typeof(+),typeof(-)},
+    ::Type{<:_Variable}, S::Type{<:_Decision})
     return DAE
 end
-function MA.promote_operation(::Union{typeof(+), typeof(-)},
-                               S::Type{<:_Decision}, ::Type{<:_Variable})
+function MA.promote_operation(::Union{typeof(+),typeof(-)},
+    S::Type{<:_Decision}, ::Type{<:_Variable})
     return DAE
 end
 # _Constant--_DecisionAffOrQuadExpr
-function MA.promote_operation(::Union{typeof(+), typeof(-), typeof(*)},
-                               ::Type{<:_Constant}, S::Type{<:_DecisionAffOrQuadExpr})
+function MA.promote_operation(::Union{typeof(+),typeof(-),typeof(*)},
+    ::Type{<:_Constant}, S::Type{<:_DecisionAffOrQuadExpr})
     return S
 end
-function MA.promote_operation(::Union{typeof(+), typeof(-), typeof(*)},
-                               S::Type{<:_DecisionAffOrQuadExpr}, ::Type{<:_Constant})
+function MA.promote_operation(::Union{typeof(+),typeof(-),typeof(*)},
+    S::Type{<:_DecisionAffOrQuadExpr}, ::Type{<:_Constant})
     return S
 end
 # _Variable/_Decision--_DecisionAffOrQuadExpr
 function MA.promote_operation(
-    ::Union{typeof(+), typeof(-)}, ::Type{<:Union{_Variable, _Decision}},
+    ::Union{typeof(+),typeof(-)}, ::Type{<:Union{_Variable,_Decision}},
     S::Type{<:_DecisionAffOrQuadExpr})
     return S
 end
 function MA.promote_operation(
-    ::Union{typeof(+), typeof(-)}, S::Type{<:_DecisionAffOrQuadExpr},
-    ::Type{<:Union{_Variable, _Decision}})
+    ::Union{typeof(+),typeof(-)}, S::Type{<:_DecisionAffOrQuadExpr},
+    ::Type{<:Union{_Variable,_Decision}})
     return S
 end
 # (_Variable/_Decision)AffExpr--(_Variable/_Decision)AffExpr
 function MA.promote_operation(
-    op::Union{typeof(+), typeof(-)}, ::Type{GenericAffExpr{C1, _Variable}},
-    ::Type{GenericAffExpr{C2, _Decision}}) where {C1, C2}
+    op::Union{typeof(+),typeof(-)}, ::Type{GenericAffExpr{C1,_Variable}},
+    ::Type{GenericAffExpr{C2,_Decision}}) where {C1,C2}
     C = MA.promote_operation(op, C1, C2)
     return DecisionAffExpr{C}
 end
 function MA.promote_operation(
-    op::Union{typeof(+), typeof(-)}, ::Type{GenericAffExpr{C1, _Decision}},
-    ::Type{GenericAffExpr{C2, _Variable}}) where {C1, C2}
+    op::Union{typeof(+),typeof(-)}, ::Type{GenericAffExpr{C1,_Decision}},
+    ::Type{GenericAffExpr{C2,_Variable}}) where {C1,C2}
     C = MA.promote_operation(op, C1, C2)
     return DecisionAffExpr{C}
 end
 # (_Variable/_Decision)AffExpr--_DecisionAffOrQuadExpr
 function MA.promote_operation(
-    ::Union{typeof(+), typeof(-)}, ::Type{GenericAffExpr{C,V}},
-    S::Type{_DecisionAffOrQuadExpr{C}}) where {C, V <: Union{_Variable, _Decision}}
+    ::Union{typeof(+),typeof(-)}, ::Type{GenericAffExpr{C,V}},
+    S::Type{_DecisionAffOrQuadExpr{C}}) where {C,V<:Union{_Variable,_Decision}}
     return S
 end
 function MA.promote_operation(
-    ::Union{typeof(+), typeof(-)}, S::Type{_DecisionAffOrQuadExpr{C}},
-    ::Type{GenericAffExpr{C,V}}) where {C, V <: Union{_Variable, _Decision}}
+    ::Union{typeof(+),typeof(-)}, S::Type{_DecisionAffOrQuadExpr{C}},
+    ::Type{GenericAffExpr{C,V}}) where {C,V<:Union{_Variable,_Decision}}
     return S
 end
 # _DecisionAffOrQuadExpr--_DecisionAffOrQuadExpr
-function MA.promote_operation(::Union{typeof(+), typeof(-)}, ::Type{A},
-                               ::Type{A}) where {A <: _DecisionAffOrQuadExpr}
+function MA.promote_operation(::Union{typeof(+),typeof(-)}, ::Type{A},
+    ::Type{A}) where {A<:_DecisionAffOrQuadExpr}
     return A
 end
-function MA.promote_operation(::Union{typeof(+), typeof(-)},
-                               ::Type{<:DecisionAffExpr},
-                               S::Type{<:DecisionQuadExpr})
+function MA.promote_operation(::Union{typeof(+),typeof(-)},
+    ::Type{<:DecisionAffExpr},
+    S::Type{<:DecisionQuadExpr})
     return S
 end
-function MA.promote_operation(::Union{typeof(+), typeof(-)},
-                               S::Type{<:DecisionQuadExpr},
-                               ::Type{<:DecisionAffExpr})
+function MA.promote_operation(::Union{typeof(+),typeof(-)},
+    S::Type{<:DecisionQuadExpr},
+    ::Type{<:DecisionAffExpr})
     return S
 end
 # _Variable--DecisionRef
@@ -125,39 +125,39 @@ function MA.promote_operation(::typeof(*), ::Type{<:_Decision}, ::Type{<:_Decisi
     return DQE
 end
 # _Variable/_Decision--(_Variable/_Decision)AffExpr
-function MA.promote_operation(::typeof(*), ::Type{<:_Variable}, ::Type{GenericAffExpr{C, _Decision}}) where {C, D <: _Decision}
+function MA.promote_operation(::typeof(*), ::Type{<:_Variable}, ::Type{GenericAffExpr{C,_Decision}}) where {C<:_Decision}
     return DecisionQuadExpr{C}
 end
-function MA.promote_operation(::typeof(*), ::Type{GenericAffExpr{C, D}}, ::Type{<:_Variable}) where {C, D <: _Decision}
+function MA.promote_operation(::typeof(*), ::Type{GenericAffExpr{C,D}}, ::Type{<:_Variable}) where {C,D<:_Decision}
     return DecisionQuadExpr{C}
 end
-function MA.promote_operation(::typeof(*), ::Type{<:_Decision}, ::Type{GenericAffExpr{C, V}}) where {C, V <: _Variable}
+function MA.promote_operation(::typeof(*), ::Type{<:_Decision}, ::Type{GenericAffExpr{C,V}}) where {C,V<:_Variable}
     return DecisionQuadExpr{C}
 end
-function MA.promote_operation(::typeof(*), ::Type{GenericAffExpr{C, V}}, ::Type{<:_Decision}) where {C, V <: _Variable}
+function MA.promote_operation(::typeof(*), ::Type{GenericAffExpr{C,V}}, ::Type{<:_Decision}) where {C,V<:_Variable}
     return DecisionQuadExpr{C}
 end
-function MA.promote_operation(::typeof(*), ::Type{D}, ::Type{GenericAffExpr{C, D}}) where {C, D <: _Decision}
+function MA.promote_operation(::typeof(*), ::Type{D}, ::Type{GenericAffExpr{C,D}}) where {C,D<:_Decision}
     return DecisionQuadExpr{C}
 end
-function MA.promote_operation(::typeof(*), ::Type{GenericAffExpr{C, D}}, ::Type{D}) where {C, D <: _Decision}
+function MA.promote_operation(::typeof(*), ::Type{GenericAffExpr{C,D}}, ::Type{D}) where {C,D<:_Decision}
     return DecisionQuadExpr{C}
 end
 # _Variable/_Decision--DecisionAffExpr
-function MA.promote_operation(::typeof(*), ::Type{V}, ::Type{DecisionAffExpr{C}}) where {C, V <: Union{_Variable, _Decision}}
+function MA.promote_operation(::typeof(*), ::Type{V}, ::Type{DecisionAffExpr{C}}) where {C,V<:Union{_Variable,_Decision}}
     return DecisionQuadExpr{C}
 end
-function MA.promote_operation(::typeof(*), ::Type{DecisionAffExpr{C}}, ::Type{V}) where {C, V <: Union{_Variable, _Decision}}
+function MA.promote_operation(::typeof(*), ::Type{DecisionAffExpr{C}}, ::Type{V}) where {C,V<:Union{_Variable,_Decision}}
     return DecisionQuadExpr{C}
 end
 
-function MA.scaling(aff::DecisionAffExpr{C}) where C
+function MA.scaling(aff::DecisionAffExpr{C}) where {C}
     if !isempty(aff.variables.terms) || !isempty(aff.decisions.terms)
         throw(InexactError("Cannot convert `$aff` to `$C`."))
     end
     return MA.scaling(aff.variables.constant)
 end
-function MA.scaling(quad::DecisionQuadExpr{C}) where C
+function MA.scaling(quad::DecisionQuadExpr{C}) where {C}
     if !isempty(quad.variables.terms) || !isempty(quad.decisions.terms) || !isempty(quad.decisions.quad.terms) || !isempty(quad.cross_terms)
         throw(InexactError("Cannot convert `$quad` to `$C`."))
     end
@@ -167,12 +167,12 @@ end
 # Mutable operate #
 # ========================== #
 # zero/one #
-function MA.operate!(op::Union{typeof(zero), typeof(one)}, aff::DecisionAffExpr)
+function MA.operate!(op::Union{typeof(zero),typeof(one)}, aff::DecisionAffExpr)
     MA.operate!(op, aff.variables)
     MA.operate!(op, aff.decisions)
     return aff
 end
-function MA.operate!(op::Union{typeof(zero), typeof(one)}, quad::DecisionQuadExpr)
+function MA.operate!(op::Union{typeof(zero),typeof(one)}, quad::DecisionQuadExpr)
     MA.operate!(op, quad.variables)
     MA.operate!(op, quad.decisions)
     empty!(quad.cross_terms)
@@ -206,22 +206,22 @@ end
 function MA.operate!(::typeof(MA.sub_mul), expr::_DecisionAffOrQuadExpr, x::_Scalar, y::_Scalar)
     return JuMP.add_to_expression!(expr, -x, y)
 end
-function MA.operate!(::typeof(MA.sub_mul), expr::_DecisionAffOrQuadExpr, x::Union{_Variable, _Decision}, y::_Constant)
+function MA.operate!(::typeof(MA.sub_mul), expr::_DecisionAffOrQuadExpr, x::Union{_Variable,_Decision}, y::_Constant)
     return JuMP.add_to_expression!(expr, x, -y)
 end
 function MA.operate!(op::MA.AddSubMul, expr::_DecisionAffOrQuadExpr, x, y)
     return MA.operate!(op, expr, x * y)
 end
-@generated function _add_sub_mul_reorder!(op::MA.AddSubMul, expr::_DecisionAffOrQuadExpr, args::Vararg{Any, N}) where N
+@generated function _add_sub_mul_reorder!(op::MA.AddSubMul, expr::_DecisionAffOrQuadExpr, args::Vararg{Any,N}) where {N}
     n = length(args)
     @assert n ≥ 3
-    varidx = findall(t -> (t <: Union{_Variable, _Decision}), collect(args))
+    varidx = findall(t -> (t <: Union{_Variable,_Decision}), collect(args))
     allscalar = all(t -> (t <: _Constant), args[setdiff(1:n, varidx)])
     idx = (allscalar && length(varidx) == 1) ? varidx[1] : n
     coef = Expr(:call, :*, [:(args[$i]) for i in setdiff(1:n, idx)]...)
     return :(MA.operate!(op, expr, $coef, args[$idx]))
 end
-function MA.operate!(op::MA.AddSubMul, expr::_DecisionAffOrQuadExpr, x, y, z, other_args::Vararg{Any, N}) where N
+function MA.operate!(op::MA.AddSubMul, expr::_DecisionAffOrQuadExpr, x, y, z, other_args::Vararg{Any,N}) where {N}
     return _add_sub_mul_reorder!(op, expr, x, y, z, other_args...)
 end
 function JuMP.add_to_expression!(expr::_DecisionAffOrQuadExpr, α::_Constant, β::_Constant)
